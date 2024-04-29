d5a21b3f3bb39e564fafa9ad99e8cb8b56411676
Eduardo Wanderley de Siqueira Araújo <wanderley.eduardo@gmail.com>
2024-04-29 00:26:08 -0300

    Add seeds for blogs

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Blog.create(title: "First seed Blog", body: "The body of the first seed blog") 
+Blog.create(title: "second seed Blog", body: "The body of the second seed blog") 
